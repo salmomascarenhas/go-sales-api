4bf58963befbc7189c009e4241c7d7646e77ab72
salmomascarenhas <salmo.cruz@gmail.com>
2024-06-19 23:09:48 -0300

    feat:✨  add Spot struct to represent event spots

---
+++ internal/events/domain/spots.go
@@ -0,0 +1,17 @@
+package domain
+
+type SpotStatus string 
+
+const (
+	SpotStatusAvailable SpotStatus = "available"
+	SpotStatusSold SpotStatus = "sold"
+)
+
+type Spot struct {
+	ID string
+	EventID string
+	Name string
+	Status SpotStatus
+	TicketID string
+
+}
