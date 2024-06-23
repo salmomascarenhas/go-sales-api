97509fa958de2770683d99964800f1b452b5fcf8
salmomascarenhas <salmo.cruz@gmail.com>
2024-06-23 01:18:24 -0300

    feat: :sparkles: implements ticket method
    implements method for calculate price of ticket and validate type ticket

--- internal/events/domain/ticket.go
+++ internal/events/domain/ticket.go
@@ -1,6 +1,12 @@
 package domain
 
+import "errors"
+
 type TicketType string
+
+var (
+	ErrInvalidTicketType = errors.New("invalid ticket type")
+)
 
 const (
 	TicketTypeHalf TicketType = "half"
@@ -13,4 +19,24 @@ type Ticket struct {
 	Spot 	*Spot
 	TicketType TicketType
 	Price float64
+}
+
+func IsValidTicketType(ticketType TicketType) bool {
+	return ticketType == TicketTypeHalf || ticketType == TicketTypeFull
+}
+
+func (t *Ticket) CalculatePrice() {
+	if t.TicketType == TicketTypeHalf {
+		t.Price /= 2  
+	}
+}
+
+func (t *Ticket) Validate() error {
+	if t.Price <= 0 {
+		return ErrPriceMustBeGreaterThanZero
+	}
+	if !IsValidTicketType(t.TicketType) {
+		return ErrInvalidTicketType
+	}
+	return nil
 }
