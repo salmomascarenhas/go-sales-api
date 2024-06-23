ee5a5bf77199f8efea4d5189de0b0afb42d93d95
salmomascarenhas <salmo.cruz@gmail.com>
2024-06-23 12:53:31 -0300

    feat: Refactor ticket type to ticket kind
    Refactor the `TicketType` to `TicketKind` in the `Ticket` struct and related functions. This change aligns with the established convention in the repository to use "kind" instead of "type" for this context.

--- internal/events/domain/ticket.go
+++ internal/events/domain/ticket.go
@@ -2,31 +2,31 @@ package domain
 
 import "errors"
 
-type TicketType string
+type TicketKind string
 
 var (
-	ErrInvalidTicketType = errors.New("invalid ticket type")
+	ErrInvalidTicketKind = errors.New("invalid ticket kind")
 )
 
 const (
-	TicketTypeHalf TicketType = "half"
-	TicketTypeFull TicketType = "full"
+	TicketKindHalf TicketKind = "half"
+	TicketKindFull TicketKind = "full"
 )
 
 type Ticket struct {
 	ID string
 	EventID string
 	Spot 	*Spot
-	TicketType TicketType
+	TicketKind TicketKind
 	Price float64
 }
 
-func isValidTicketType(ticketType TicketType) bool {
-	return ticketType == TicketTypeHalf || ticketType == TicketTypeFull
+func isValidTicketKind(ticketKind TicketKind) bool {
+	return ticketKind == TicketKindHalf || ticketKind == TicketKindFull
 }
 
 func (t *Ticket) CalculatePrice() {
-	if t.TicketType == TicketTypeHalf {
+	if t.TicketKind == TicketKindHalf {
 		t.Price /= 2  
 	}
 }
@@ -35,8 +35,8 @@ func (t *Ticket) Validate() error {
 	if t.Price <= 0 {
 		return ErrPriceMustBeGreaterThanZero
 	}
-	if !isValidTicketType(t.TicketType) {
-		return ErrInvalidTicketType
+	if !isValidTicketKind(t.TicketKind) {
+		return ErrInvalidTicketKind
 	}
 	return nil
 }
