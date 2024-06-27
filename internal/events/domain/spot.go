8c82edf0a7a70c5731a60215ce5348a44382aba1
salmomascarenhas <salmo.cruz@gmail.com>
2024-06-26 23:05:09 -0300

    feat: Refactor spot validation logic and error messages
    Refactor the `Spot` struct's validation logic and error messages for improved clarity and consistency. Update the error messages to be more descriptive and specific, such as "spot name is required" and "spot name must start with a letter". This change enhances the readability and maintainability of the codebase.

--- internal/events/domain/spot.go
+++ internal/events/domain/spot.go
@@ -6,59 +6,60 @@ import (
 	"github.com/google/uuid"
 )
 
-type SpotStatus string 
-
 var (
-	ErrSpotNameIsRequired = errors.New("spot name is required")
-	ErrSpotNotFound = errors.New("spot not found")
+	ErrInvalidSpotNumber   = errors.New("invalid spot number")
+	ErrSpotNotFound        = errors.New("spot not found")
 	ErrSpotAlreadyReserved = errors.New("spot already reserved")
-	ErrSpotNameMustBeAtLeast2CharactersLong = errors.New("spot name must be at least 2 characters long")
-	ErrSpotNameMustStartWithACapitalLetter = errors.New("spot name must start with a capital letter")
-	ErrSpotNameSecondCharacterMustBeANumber = errors.New("spot name second character must be a number")
 )
+
+type SpotStatus string
 
 const (
 	SpotStatusAvailable SpotStatus = "available"
-	SpotStatusSold SpotStatus = "sold"
+	SpotStatusSold      SpotStatus = "sold"
 )
 
 type Spot struct {
-	ID string
-	EventID string
-	Name string
-	Status SpotStatus
+	ID       string
+	EventID  string
+	Name     string // all name uses the rule: Letter+Number. Ex: A1, B2, C3, etc.
+	Status   SpotStatus
 	TicketID string
 }
 
-func NewSpot(e *Event, name string) (*Spot, error) {
-	s := &Spot{
-		ID: uuid.New().String(),
-		EventID: e.ID,
-		Name: name,
-		Status: SpotStatusAvailable,
+// NewSpot creates a new spot with the given parameters.
+func NewSpot(event *Event, name string) (*Spot, error) {
+	spot := &Spot{
+		ID:      uuid.New().String(),
+		EventID: event.ID,
+		Name:    name,
+		Status:  SpotStatusAvailable,
 	}
-	if err:= s.Validate(); err != nil {
+	if err := spot.Validate(); err != nil {
 		return nil, err
 	}
-	return s, nil
+	return spot, nil
 }
 
-func (s Spot) Validate() error {
+// Validate checks if the spot data is valid.
+func (s *Spot) Validate() error {
 	if len(s.Name) == 0 {
-		return ErrSpotNameIsRequired
+		return errors.New("spot name is required")
 	}
 	if len(s.Name) < 2 {
-		return ErrSpotNameMustBeAtLeast2CharactersLong
+		return errors.New("spot name must be at least 2 characters long")
 	}
+	// Validate if the spot name is in the correct format
 	if s.Name[0] < 'A' || s.Name[0] > 'Z' {
-		return ErrSpotNameMustStartWithACapitalLetter
+		return errors.New("spot name must start with a letter")
 	}
 	if s.Name[1] < '0' || s.Name[1] > '9' {
-		return ErrSpotNameSecondCharacterMustBeANumber
+		return errors.New("spot name must end with a number")
 	}
 	return nil
 }
 
+// Reserve reserves the spot for the given ticket ID.
 func (s *Spot) Reserve(ticketID string) error {
 	if s.Status == SpotStatusSold {
 		return ErrSpotAlreadyReserved
