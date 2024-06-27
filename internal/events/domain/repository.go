36dd37df7731f95b3854419ec4a4685c16c83d4a
salmomascarenhas <salmo.cruz@gmail.com>
2024-06-26 22:46:12 -0300

    refactor: Update EventRepository interface methods
    Update the `EventRepository` interface methods to follow naming conventions and improve clarity. Renamed `ListsEvents` to `ListEvents`, `FindByEventID` to `FindEventByID`, and `FindSpotByName` to `FindSpotByName`. Also added a new method `CreateEvent` and updated the method signature for `ReserveSpot`.

--- internal/events/domain/repository.go
+++ internal/events/domain/repository.go
@@ -1,12 +1,12 @@
 package domain
 
 type EventRepository interface {
-	ListsEvents() ([]*Event, error)
-	FindByEventID(id string) (*Event, error)
+	ListEvents() ([]Event, error)
+	FindEventByID(eventID string) (*Event, error)
 	FindSpotsByEventID(eventID string) ([]*Spot, error)
-	FindSpotByName(eventID, name string) (*Spot, error)
-	// CreateEvent(event *Event) error
+	FindSpotByName(eventID, spotName string) (*Spot, error) // Atualizado
+	CreateEvent(event *Event) error
 	CreateSpot(spot *Spot) error
 	CreateTicket(ticket *Ticket) error
-	ReserveSpot(spotID string, ticketID string) error
+	ReserveSpot(spotID, ticketID string) error
 }
